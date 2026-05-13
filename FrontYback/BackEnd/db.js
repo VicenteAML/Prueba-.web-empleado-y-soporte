09cb5d5d7eb1a983e5831baebff52176a144535a
VicenteAML <vicenteandresmiranda0211@gmail.com>
2026-05-13 11:58:40 -0400

    act 3

--- FrontYback/BackEnd/db.js
+++ FrontYback/BackEnd/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'jujutsuSERVER9877##',
+  password: 'admin',
   database: 'datadb',
   waitForConnections: true,
   connectionLimit: 10,
